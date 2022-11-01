3c2d5f12824aa1f86644265757b89c41c8a4f751
BNHHNnyx <seregafrom58@gmail.com>
2022-11-01 14:55:32 +0300

    commit

--- src/test3.java
+++ src/test3.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 import java.util.Scanner;
 
 public class test3 {
@@ -6,10 +5,6 @@ public class test3 {
         Scanner scanner = new Scanner(System.in);
         String inputStr = scanner.nextLine();
         int signCount = 0;
-        int romanOrArabic = 0;                                                                 // 0 - roman, 1 - arabic
-        if(inputStr.contains("I") || inputStr.contains("V") || inputStr.contains("X")) {
-            romanOrArabic++;
-        }
         String clearedStr = inputStr.replaceAll(" ", "");
         String[] array = new String[10];
         if(clearedStr.contains("+")) {                                                         // creates array
@@ -21,14 +16,56 @@ public class test3 {
         } else if(clearedStr.contains("*")) {
             array = clearedStr.split("\\*");
         }
-        int operand1 = Integer.parseInt(array[0]);                                             // operands
-        if(operand1 > 10){
-            throw new Exception();
+
+        int romanOrArabic = 0;                                                                 // 0 - roman, 2 - arabic
+        if(array[0].contains("I") || array[0].contains("V") || array[0].contains("X") ) {
+            romanOrArabic++;
+            System.out.println(romanOrArabic);
         }
-        int operand2 = Integer.parseInt(array[1]);
-        if(operand2 > 10){
-            throw new Exception();
+        if(array[1].contains("I") || array[1].contains("V") || array[1].contains("X")) {
+            romanOrArabic++;
+            System.out.println(romanOrArabic);
         }
+
+        System.out.println(romanOrArabic);
+
+        String [] arab = {"10","1","2","3","4","5","6","7","8","9"};
+        String [] rome = {"X","I","II","III","IV","V","VI","VII","VIII","IX"};
+        int operand1 = 0;
+        int operand2 = 0;
+        if(romanOrArabic == 2){
+            int x = 0;
+            int i = 0;
+            while(x < 1){
+                if(array[0].contains(rome[i])) {
+                    x++;
+                    operand1 = Integer.parseInt(arab[i]);
+                }
+                i++;
+            }
+            x = 0;
+            i = 0;
+            while(x < 1){
+                if(array[0].contains(rome[i])) {
+                    x++;
+                    operand2 = Integer.parseInt(arab[i]);
+                }
+                i++;
+            }
+        }
+        else if(romanOrArabic == 0) {
+            operand1 = Integer.parseInt(array[0]);                                             // operands
+            if (operand1 > 10) {
+                throw new Exception();
+            }
+            operand2 = Integer.parseInt(array[1]);
+            if (operand2 > 10) {
+                throw new Exception();
+            }
+        }
+            else if(romanOrArabic != 2 && romanOrArabic != 0) {
+                throw new Exception();
+            }
         String addStr = inputStr.replace("+", "");           // dunno, sort of sign(+, -, /, *) counter
         String substractStr = inputStr.replace("-", "");
         String multiplyStr = inputStr.replace("*", "");
@@ -51,6 +88,5 @@ public class test3 {
         } else {
             throw new Exception();
         }
-
     }
 }
